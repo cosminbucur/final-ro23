a86dcd7f1334596cc4999ff37ddce8eb88ba8bc8
cosmin bucur <cosmin.bucur@kambi.com>
2021-10-19 21:36:17 +0300

    DEV-1 prepare spring boot app with mysql configuration

--- src/main/java/com/sda/project/Application.java
+++ src/main/java/com/sda/project/Application.java
@@ -1,8 +1,12 @@
 package com.sda.project;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        SpringApplication.run(Application.class, args);
     }
 }
---
+++ src/main/java/com/sda/project/recap/MainClass.java
@@ -0,0 +1,11 @@
+package com.sda.project.recap;
+
+public class MainClass {
+
+    // javac Application.java -> Application.class
+    // java Application 50 cosmin
+    public static void main(String[] args) {
+        System.out.println(args[0]);    // 50
+        System.out.println(args[1]);    // "cosmin"
+    }
+}
